5070743ad0bc827b87b530a492157b9f503b66cc
Ashutoshbhatiya <amar@aleaitsolutions.in>
2016-03-31 16:25:05 +0530

    upload
    an index very good

---
+++ index.php
@@ -0,0 +1,304 @@
+   <img src="img/Real Computer.jpg" width="1000" height="110">
+ <!DOCTYPE html>
+<html>
+<body>
+   <img src="img/Logo-Design-Target.jpg"width="200" height="120" style="float:right;">
+ <p title="About Real Computer academy">
+<h1 style="color:blue;">Real Computer Academy is a very Good Center of Computer Study.</h1> <br>
+<h2 style="color:red; font-size:27px;">It provides tutorials and references covering many aspects of web programming.</h2> <br>
+<h3 style="color:blue;font-size:25px;">Hardware & Software</h3> <br>
+<h4 style="color:yellow;font-size:25px;">Including HTML, CSS, JavaScript, XML, SQL, PHP, ASP, etc.</h4><br>
+ This is Cyberoam link:- <a href="http://192.168.1.1/corporate/webpages/login.jsp">This is a Cyberoam link</a> <br> <br>
+ <body style="background-color:green;">
+ <table border="1" style="width:100%">
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>	
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+</table> <br>
+<table border="1" style="width:100%">
+   <tr>
+      <td>amar singh</td>
+	  <td>anand</td>
+	  <td>adsad</td>
+	  </tr>
+	  <tr>
+	  <td>singh</td>
+	  <td>kal</td>
+	  <td>ajar</td>
+	  </tr>
+	  <tr>
+	  <td>am</td>
+	  <td>ajars</td>
+	  <td>kamal</td> <br>
+ <table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th colspan="2">Telephone</th>
+  </tr>
+  <tr>
+    <td>Bill Gates</td>
+    <td>555 77 854</td>
+    <td>555 77 855</td>
+  </tr>
+</table>
+<h2 style="color:red; font-size:25px;">Amar Cafee and full cool</h2> <br>
+<ul>
+  <li>Coffee full</li>
+  <li>Tea some</li>
+  <li>Milk jat</li>
+</ul>  
+<h2 style="color:blue; font-size:30px;">Jai Amar Hotel of Jaipur Gulab Bagh</h2> <br>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul> 
+ <ul style="list-style-type:square">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul> 
+ <ul style="list-style-type:none">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ul> 
+ <ol>
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol> 
+ <ol type="A">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol> 
+ <ol type="a">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol> 
+ <ol type="I">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol> 
+ <ol type="i">
+  <li>Coffee</li>
+  <li>Tea</li>
+  <li>Milk</li>
+</ol> 
+ <dl>
+  <dt>Coffee</dt>
+  <dd>- black hot drink</dd>
+  <dt>Milk</dt>
+  <dd>- white cold drink</dd>
+</dl> 
+ <ul>
+  <li>Coffee</li>
+  <li>Tea
+    <ul>
+      <li>Black tea</li>
+      <li>Green tea</li>
+    </ul>
+  </li>
+  <li>Milk</li>
+</ul> 
+</style>
+</head>
+<body>
+<h2>Real Computer Academy</h2>
+<ul id="menu">
+  <li>HTML</li>
+  <li>CSS</li>
+  <li>JavaScript</li>
+  <li>PHP</li>
+</ul> 
+<div style="background-color:yellow; color:white; padding:27px;">
+<h2 style="27px;">London</h2>
+<p>London is the capital city of England. It is the most populous city in the United<br> Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+<p>Standing on the River Thames, London has been a major settlement for two millennia, <br>its history going back to its founding by the Romans, who named it Londinium.</p>
+<p> Amar  singh choudhary is a owner of center of Computer.</p>
+</div> 
+<h1>Indra<span style="color:blue">Jai</span>Amar </h1>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+#header {
+    background-color:black;
+    color:white;
+    text-align:center;
+    padding:5px;
+}
+#nav {
+    line-height:30px;
+    background-color:#eeeeee;
+    height:300px;
+    width:100px;
+    float:left;
+    padding:5px;	      
+}
+#section {
+    width:350px;
+    float:left;
+    padding:10px;	 	 
+}
+#footer {
+    background-color:yellow;
+    color:green;
+    clear:both;
+    text-align:center;
+   padding:5px;	 	 
+}
+</style>
+</head>
+<body>
+
+<div id="header">
+<h1>City Gallery</h1>
+</div>
+<div id="section">
+<h2>Jaipur</h2>
+<p>Jaipur is the capital city of India. It is the most populous city in the Rajasthan,
+with a metropolitan area of over 13 million inhabitants.</p>
+<p>Standing on the River Thames, Jaipur has been a major settlement for two millennia,
+its history going back to its founding by the Romans, who named it Jaipur.</p>
+</div>
+</body>
+</html>
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<style>
+.city {
+   float: left;
+   margin: 10px;
+   padding: 10px;
+   width: 300px;
+   height: 300px;
+   border: 1px solid yellow;
+}   
+</style>
+</head>
+<body>
+
+<h1>Amar singh Choudhary</h1>
+<h2>Real Computer Academy!</h2>
+
+<div class="city">
+  <h2>Jaipur</h2>
+  <p>Jaipur is the capital city of India.</p>
+  <p>It is the most populous city in the Rajasthan,
+with a metropolitan area of over 13 million inhabitants.</p>
+</div>
+
+<div class="city">
+  <h2>Ajmer</h2>
+  <p>Ajmer is the capital of India.</p> 
+  <p>The Ajmer area is one of the largest population centers in India,
+with more than 12 million inhabitants.</p>
+</div>
+
+<div class="city">
+  <h2>Sikar</h2>
+  <p>Sikar is the capital of India.</p>
+  <p>It is the center of the Greater Sikar Area,
+and the most populous metropolitan area in the world.</p>
+</div>
+
+<div class="city">
+  <h2>View Source</h2>
+  <p>View the source code to see how this page was built.</p>
+</div>
+
+</body>
+</html>
+<div id="footer">
+Copyright © Amar singh Choudhary
+</div>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.cyberoam.com/contactus.html">
+<body>
+
+<div class="w3-container w3-orange">
+  <h1>Cyberoam Demo.com</h1>      
+  <p>Resize this responsive page!</p>      
+</div>
+
+<div class="Demo-row-padding">
+
+<div class="Demo-third">
+  <h2>India</h2>
+  <p>India is the capital city of Pakistan.</p>
+  <p>It is the most populous city in the U.S,
+  with a metropolitan area of over 13 million inhabitants.</p>
+</div>
+
+<div class=Demo-third">
+  <h2>Kajakistan</h2>
+  <p>Paris is the capital of France.</p> 
+  <p>The Paris area is one of the largest population centers in Europe,
+  with more than 12 million inhabitants.</p>
+</div>
+
+<div class="w3-third">
+  <h2>Tokyo</h2>
+  <p>Tokyo is the capital of Japan.</p>
+  <p>It is the center of the Greater Tokyo Area,
+  and the most populous metropolitan area in the world.</p>
+</div>
+
+</div>
+
+</body>
+</html>
+<script>
+document.getElementById("demo").innerHTML = "Hello JavaScript!";
+</script> 
+<script>
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello Amar singh kya kar rahe ho kaise ho how are sir i am fine sir your birthday kab hai";
+}
+</script>
+
+<body>
+
+<h1>Demo On Comments of Amar singh</h1>
+
+<p id="demo">A Paragraph</p>
+
+<button type="button" onclick="myFunction()">Try it</button>
+
+</body>
+<!DOCTYPE html>
+<html>
+<body>
+
+<form>
+  User Name:<br>
+  <input type="text" name="User Name">
+  <br>
+  Password:<br>
+  <input type="text" name="Password">
+</form>
+
+<p>Note that the form itself is not visible.</p>
+
+<p>Also note that the default width of a text input field is 20 characters.</p>
+
+</body>
+</html>
+<form>
+  <input type="radio" name="gender" value="male" checked> Male<br>
+  <input type="radio" name="gender" value="female"> Female<br>
+  <input
+  type="radio" name="gender" value="other"> Other  
+</form> 
